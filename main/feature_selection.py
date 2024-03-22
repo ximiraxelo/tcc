4092e97f4e2824e44241bd1dfa0e74ed4bb10ae2
Esdras Battosti <esdrasbattostisilva@gmail.com>
2024-03-22 09:50:47 -0300

    abstracting feature selection into a function

--- main/feature_selection.py
+++ main/feature_selection.py
@@ -4,34 +4,53 @@ from pathlib import Path
 import pandas as pd
 from rich import print
 
-DATA_PATH = Path('D:/dados_tcc/dados_tcc_esdras')
-SAVE_PATH = Path('D:/dados_tcc/files')
 
-if not SAVE_PATH.exists():
-    SAVE_PATH.mkdir()
+def non_zero_variance_selector(
+    data_path, save_path=None, save=False, threshold=1e-6
+):
 
-filtered_columns = []
+    selected_features = []
 
-for column in range(730):
+    for column in range(730):
 
-    feature_list = []
+        feature_list = []
 
-    for dataset_path in DATA_PATH.glob('**/X.feather'):
-        feature_chunk = pd.read_feather(
-            dataset_path, columns=[column]
-        ).squeeze()
-        feature_list.append(feature_chunk)
+        for dataset_path in data_path.glob('**/X.feather'):
+            feature_chunk = pd.read_feather(
+                dataset_path, columns=[column]
+            ).squeeze()
+            feature_list.append(feature_chunk)
 
-    feature = pd.concat(feature_list, ignore_index=True)
+        feature = pd.concat(feature_list, ignore_index=True)
 
-    print(f'[{column:3}] {feature.name:35} | ', end='')
+        print(f'[{column:3}] {feature.name:35} | ', end='')
 
-    # Non zero variance and not null features
-    if feature.var() > 1e-6 and feature.notna().all():
-        filtered_columns.append(column)
-        print('[green]OK')
-    else:
-        print('[red]REMOVED')
+        # Non zero variance and not null features
+        if feature.var() > threshold and feature.notnull().all():
+            selected_features.append(column)
+            print('[green]OK')
+        else:
+            print('[red]REMOVED')
 
-with open(SAVE_PATH / 'non_zero_variance_columns.pickle', 'wb') as file:
-    pickle.dump(filtered_columns, file)
+    if save:
+        with open(
+            save_path / 'non_zero_variance_features.pickle', 'wb'
+        ) as file:
+            pickle.dump(selected_features, file)
+
+        print('\n[green]---- Successfully saved ----\n')
+
+    return selected_features
+
+
+if __name__ == '__main__':
+
+    DATA_PATH = Path('D:/dados_tcc/dados_tcc_esdras')
+    SAVE_PATH = Path('D:/dados_tcc/files')
+
+    if not SAVE_PATH.exists():
+        SAVE_PATH.mkdir()
+
+    non_zero_variance_features = non_zero_variance_selector(
+        DATA_PATH, SAVE_PATH, save=True
+    )
